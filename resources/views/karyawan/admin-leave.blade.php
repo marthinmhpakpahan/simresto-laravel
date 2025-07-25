d61f1b99898398ca651d31f420f21c126b2ee051
Marthin Pakpahan <marthinpakpahan@Marthins-MacBook-Pro.local>
2025-07-25 20:31:36 +0700

    add modal for accept and decline leave

--- resources/views/karyawan/admin-leave.blade.php
+++ resources/views/karyawan/admin-leave.blade.php
@@ -38,8 +38,8 @@
                                             <td class="font-weight-bolder text-uppercase">{{ $leave->status }}</td>
                                             <td>
                                                 @if ($leave->status == "Belum Diproses")
-                                                    <a href="{{ route("karyawan.leave_action", [$leave->id, 'Diterima']) }}"><i class="fa fa-check btn btn-xs btn-success"></i></a>
-                                                    <a href="{{ route("karyawan.leave_action", [$leave->id, 'Ditolak']) }}"><i class="fa fa-close btn btn-xs btn-danger"></i></a>
+                                                    <a class="btn-accept" data-bs-toggle="modal" data-bs-target="#modalAcceptConfirmation" data-url="{{ route("karyawan.leave_action", [$leave->id, 'Diterima']) }}" href="#"><i class="fa fa-check btn btn-xs btn-success"></i></a>
+                                                    <a class="btn-decline" data-bs-toggle="modal" data-bs-target="#modalDeclineConfirmation" data-url="{{ route("karyawan.leave_action", [$leave->id, 'Ditolak']) }}" href="#"><i class="fa fa-close btn btn-xs btn-danger"></i></a>
                                                 @endif
                                             </td>
                                         </tr>
@@ -53,4 +53,123 @@
         </div>
     </div>
 </main>
+<div class="modal fade" id="modalAcceptConfirmation" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-danger text-center">
+                <h1 class="modal-title fs-5 text-center text-white" id="staticBackdropLabel">Konfirmasi</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <h4>Apakah anda yakin ingin menyetujui Pengajuan Cuti ini?</h4><br />
+                <table>
+                    <tr>
+                        <td>Nama Karyawan</td>
+                        <td>&nbsp;&nbsp;&nbsp;</td>
+                        <td>&nbsp;:&nbsp;</td>
+                        <td><span class="modal-accept-title"></span></td>
+                    </tr>
+                    <tr>
+                        <td>Judul</td>
+                        <td>&nbsp;&nbsp;&nbsp;</td>
+                        <td>&nbsp;:&nbsp;</td>
+                        <td><span class="modal-accept-title"></span></td>
+                    </tr>
+                    <tr>
+                        <td>Tanggal Mulai</td>
+                        <td>&nbsp;&nbsp;&nbsp;</td>
+                        <td>&nbsp;:&nbsp;</td>
+                        <td><span class="modal-accept-start-date"></span></td>
+                    </tr>
+                    <tr>
+                        <td>Tanggal Selesai</td>
+                        <td>&nbsp;&nbsp;&nbsp;</td>
+                        <td>&nbsp;:&nbsp;</td>
+                        <td><span class="modal-accept-end-date"></span></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                <a class="modal-btn-confirm-accept" href=""><button type="button"
+                        class="btn btn-danger">Yakin!</button></a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="modalDeclineConfirmation" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header bg-danger text-center">
+                <h1 class="modal-title fs-5 text-center text-white" id="staticBackdropLabel">Konfirmasi</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <h4>Apakah anda yakin ingin menolak Pengajuan Cuti ini?</h4><br />
+                <table>
+                    <tr>
+                        <td>Nama Karyawan</td>
+                        <td>&nbsp;&nbsp;&nbsp;</td>
+                        <td>&nbsp;:&nbsp;</td>
+                        <td><span class="modal-decline-title"></span></td>
+                    </tr>
+                    <tr>
+                        <td>Judul</td>
+                        <td>&nbsp;&nbsp;&nbsp;</td>
+                        <td>&nbsp;:&nbsp;</td>
+                        <td><span class="modal-decline-title"></span></td>
+                    </tr>
+                    <tr>
+                        <td>Tanggal Mulai</td>
+                        <td>&nbsp;&nbsp;&nbsp;</td>
+                        <td>&nbsp;:&nbsp;</td>
+                        <td><span class="modal-decline-start-date"></span></td>
+                    </tr>
+                    <tr>
+                        <td>Tanggal Selesai</td>
+                        <td>&nbsp;&nbsp;&nbsp;</td>
+                        <td>&nbsp;:&nbsp;</td>
+                        <td><span class="modal-decline-end-date"></span></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                <a class="modal-btn-confirm-decline" href=""><button type="button"
+                        class="btn btn-danger">Yakin</button></a>
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    $(document).ready(function() {
+        console.log("Leave Karyawan Page!");
+        $(".btn-accept").on("click", function() {
+            var data_full_name = $(this).data("full-name");
+            $(".modal-accept-full-name").text(data_full_name);
+            var data_title = $(this).data("title");
+            $(".modal-accept-title").text(data_title);
+            var data_start_date = $(this).data("start-date");
+            $(".modal-accept-start-date").text(data_start_date);
+            var data_end_date = $(this).data("end-date");
+            $(".modal-accept-end-date").text(data_end_date);
+            var data_url = $(this).data("url");
+            $(".modal-btn-confirm-accept").attr("href", data_url);
+        });
+        $(".btn-decline").on("click", function() {
+            var data_full_name = $(this).data("full-name");
+            $(".modal-decline-full-name").text(data_full_name);
+            var data_title = $(this).data("title");
+            $(".modal-decline-title").text(data_title);
+            var data_start_date = $(this).data("start-date");
+            $(".modal-decline-start-date").text(data_start_date);
+            var data_end_date = $(this).data("end-date");
+            $(".modal-decline-end-date").text(data_end_date);
+            var data_url = $(this).data("url");
+            $(".modal-btn-confirm-decline").attr("href", data_url);
+        });
+    });
+</script>
 @include('dashboard.footer')
